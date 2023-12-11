0c3851d3112d076cc1d45a2dd67892fa859d4e1a
shiruten <shiruten1215@gmail.com>
2023-12-11 09:12:44 +0900

    ✨ SideBarItemコンポーネント実装

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,17 +1,25 @@
+import { twMerge } from "tailwind-merge";
+
 type BoxProps = {
   children: React.ReactNode;
   className?: string;
 };
 
-const Box: React.FC<BoxProps> = ({
-  children,
-  className,
-}) => {
+const Box: React.FC<BoxProps> = ({ children, className }) => {
   return (
-    <div>
+    <div
+      className={twMerge(`
+        bg-neutral-900
+        rounded-lg
+        h-fit
+        w-full
+      `,
+        className
+      )}
+    >
       {children}
     </div>
   );
-}
+};
 
 export default Box;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,7 +4,9 @@ import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+
 import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 
 type SidebarProps = {
   children: React.ReactNode;
@@ -45,9 +47,25 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
         "
       >
         <Box>
-          Sidebar Navigation
+          <div
+            className="
+            flex
+            flex-col
+            gap-y-4
+            px-5
+            py-4
+          "
+          >
+            {routes.map((item) => (
+              <SidebarItem key={item.label} {...item} />
+            ))}
+          </div>
         </Box>
+        <Box className="overflow-y-auto h-full">Song Library</Box>
       </div>
+      <main className="h-full flex-1 overflow-auto py-2">
+        {children}
+      </main>
     </div>
   );
 };
