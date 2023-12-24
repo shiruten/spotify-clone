d6803e1d283cc4a11687d222b8700544f36d2019
shiruten <shiruten1215@gmail.com>
2023-12-24 16:47:11 +0900

    ✨ Libraryコンポーネント実装

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import { BiSearch } from "react-icons/bi";
 
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 type SidebarProps = {
   children: React.ReactNode;
@@ -61,7 +62,9 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
             ))}
           </div>
         </Box>
-        <Box className="overflow-y-auto h-full">Song Library</Box>
+        <Box className="overflow-y-auto h-full">
+          <Library />
+        </Box>
       </div>
       <main className="h-full flex-1 overflow-auto py-2">
         {children}
